a55ca365839f7068d4f9ca6837bbed1febeccd84
rangikufan999 <rangikufan999@gmail.com>
2019-03-26 14:41:56 -0500

    Add files via upload

---
+++ kanji.js
@@ -0,0 +1,121 @@
+//Global variables needed at start of quiz
+var quizIndex = 0;
+var quizCurrentScore = 0;
+var quizSize = 10;
+var quiz = "";
+var quizInd = 0;
+
+
+//This function starts a new quiz, replaces the start quiz button with a restart quiz button, and fills up the first query and answers div.
+function startQuiz(){
+	//Reset Quiz
+	quizInd = 0;
+	quizCurrentScore = 0;
+	updateScore();
+	quiz = "";
+
+	//reset rightOrWrong
+	document.getElementById("rightOrWrong").innerHTML = " ";
+
+	//Get query header
+	var queryHeader = document.getElementById("kanjiQuery");
+
+	quiz = createQuizQuestions();
+
+	queryHeader.innerHTML = quiz[0].kanjiDef;
+
+	var answers = createAnswers(quiz, quizInd);
+	fillAnswers(answers);
+
+}
+
+function createQuizQuestions(){
+	var returnArray = [];
+
+	for(var i = 0; i< quizSize;i++){
+		returnArray.push(kanjiLibrary[kanjiIndex[i]]);
+	}
+
+	shuffle(returnArray);
+	return returnArray;
+}
+
+function fillAnswers(ans){
+	var ans1 = document.getElementById("answer1");
+	var ans2 = document.getElementById("answer2");
+	var ans3 = document.getElementById("answer3");
+	var ans4 = document.getElementById("answer4");
+	var ans5 = document.getElementById("answer5");
+
+	ans1.innerHTML = ans[0].kanji;
+	ans2.innerHTML = ans[1].kanji;
+	ans3.innerHTML = ans[2].kanji;
+	ans4.innerHTML = ans[3].kanji;
+	ans5.innerHTML = ans[4].kanji;
+}
+
+function checkAnswer(elem){
+	if(elem.innerHTML == quiz[quizInd].kanji){
+		document.getElementById("rightOrWrong").innerHTML = "CORRECT!";
+		quizCurrentScore += 1;
+		updateScore();
+	}else if(elem.innerHTML != quiz[quizInd].kanji){
+		document.getElementById("rightOrWrong").innerHTML = "WRONG!";
+	}
+}
+
+function updateScore(){
+	var scr = document.getElementById("currentScore");
+
+	scr.innerHTML = quizCurrentScore;
+}
+
+function nextQuestion(elem){
+	checkAnswer(elem);
+	quizInd += 1;
+	if(quizInd < 10){
+		var answers = createAnswers(quiz, quizInd);
+		var queryHeader = document.getElementById("kanjiQuery");
+		fillAnswers(answers);
+		queryHeader.innerHTML = quiz[quizInd].kanjiDef;
+	}else if(quizInd >= 10){
+		document.getElementById("rightOrWrong").innerHTML = "The quiz is finished. Click Start Quiz to retake the quiz.";
+	}
+}
+
+//Utility
+function createAnswers(quizArr, quizInd){
+	ansArr = quizArr.slice();
+
+	ansArr.splice(quizInd, 1);
+	shuffle(ansArr);
+
+	for(var i = 0;i<5;i++){
+		ansArr.pop();
+	}
+
+	ansArr.push(quizArr[quizInd]);
+	shuffle(ansArr);
+
+	return ansArr;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
---
+++ kanjiDatabase.js
@@ -0,0 +1,96 @@
+var kanjiLibrary = {
+	"中":{
+		"kanji": "中",
+		"kanjiDef": "center, middle, during",
+		"kanjiPron": "naka, chuu"
+	},
+
+	"北":{
+		"kanji": "北",
+		"kanjiDef": "North",
+		"kanjiPron": "Hoku, kita"
+	},
+
+	"金":{
+		"kanji": "金",
+		"kanjiDef": "Gold",
+		"kanjiPron": "Kin"
+	},
+
+	"道":{
+		"kanji": "道",
+		"kanjiDef": "Path, Road, Way",
+		"kanjiPron": "dou, tou, michi",
+	},
+
+	"週":{
+		"kanji": "週",
+		"kanjiDef": "Week",
+		"kanjiPron": "Shuu",
+	},
+
+	"車":{
+		"kanji": "車",
+		"kanjiDef": "Car",
+		"kanjiPron": "sha, kuruma",
+	},
+
+	"足":{
+		"kanji": "足",
+		"kanjiDef": "Foot",
+		"kanjiPron": "soku, ashi",
+	},
+
+	"買":{
+		"kanji": "買",
+		"kanjiDef": "Buy",
+		"kanjiPron": "bai, k.au",
+	},
+
+	"読":{
+		"kanji": "読",
+		"kanjiDef": "Read",
+		"kanjiPron": "Doku, toku, tou, yo.mu",
+	},
+
+	"語":{
+		"kanji": "語",
+		"kanjiDef": "Word, Speech, Language",
+		"kanjiPron": "go, kata.ru",
+	},
+
+	"話":{
+		"kanji": "話",
+		"kanjiDef": "Tale, Talk",
+		"kanjiPron": "Wa, hana.su",
+	},
+
+	"言":{
+		"kanji": "言",
+		"kanjiDef": "Say",
+		"kanjiPron": "gen, gon, i.u, koto",
+	},
+
+	"見":{
+		"kanji": "見",
+		"kanjiDef": "See, Hopes, Chances, Idea, Opinion, look at, visible",
+		"kanjiPron": "ken, mi.ru, mi.eru, mi.seru",
+	}
+}
+
+var kanjiIndex = [
+	"中", 
+	"北", 
+	"金", 
+	"道", 
+	"週", 
+	"車", 
+	"足", 
+	"買", 
+	"読", 
+	"語", 
+	"話", 
+	"言", 
+	"見"
+];
+
